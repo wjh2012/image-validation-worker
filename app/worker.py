29ae04fe59102c1594e24ddb0c84317ff2f37a29
wjh <wjh2012@naver.com>
2025-02-10 00:03:59 +0900

    mv

--- app/worker.py
+++ app/worker.py
@@ -2,22 +2,21 @@ import time
 
 import pika
 
+from app.service.blank_detector import BlankDetector
+
 RABBITMQ_URL = "amqp://admin:admin@localhost:5672"
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+def callback(ch, method, properties, body, blank_detector):
 
     time.sleep(10)
+    blank_detector.is_blank_image(body)
 
     ch.basic_publish(exchange="", routing_key="result_queue", body="complete".encode())
-    # 메시지 처리 완료 후 Ack 전송하여 큐에서 제거
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    print("ack")
 
 
 def main():
-
     params = pika.URLParameters(RABBITMQ_URL)
     connection = pika.BlockingConnection(parameters=params)
     channel = connection.channel()
@@ -25,7 +24,15 @@ def main():
     queue_name = "test"
     channel.queue_declare(queue=queue_name, durable=True)
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
+    blank_detector = BlankDetector()
+
+    channel.basic_consume(
+        queue=queue_name,
+        on_message_callback=lambda ch, method, properties, body: callback(
+            ch, method, properties, body, blank_detector
+        ),
+    )
 
     try:
         channel.start_consuming()
