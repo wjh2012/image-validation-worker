27228bc7c9bf42c99613e4c71e07d7192ae18fdb
wjh <wjh2012@naver.com>
2025-02-20 19:01:07 +0900

    fix: 큐 소비

--- blocking_app/config/minio.py
+++ blocking_app/config/minio.py
@@ -3,7 +3,8 @@ import threading
 import boto3
 from botocore.config import Config
 from botocore.exceptions import NoCredentialsError, EndpointConnectionError
-from app.config.custom_logger import logger
+
+from blocking_app.config.custom_logger import logger
 
 
 class MinioConnection:
@@ -22,16 +23,13 @@ class MinioConnection:
         if self._initialized and not override:
             return
 
-        # 환경 변수에서 MinIO 정보 가져오기
         self.minio_host = os.getenv("MINIO_HOST", "localhost")
         self.minio_port = int(os.getenv("MINIO_PORT", 9000))
         self.minio_username = os.getenv("MINIO_USERNAME", "admin")
         self.minio_password = os.getenv("MINIO_PASSWORD", "adminadmin")
 
-        # MinIO 엔드포인트 URL 생성
         self.endpoint_url = f"http://{self.minio_host}:{self.minio_port}"
 
-        # boto3 클라이언트
         self.minio_client = None
 
         if auto_connect:
@@ -40,7 +38,6 @@ class MinioConnection:
         self._initialized = True
 
     def _connect(self):
-        """MinIO 연결 시도 및 확인"""
         try:
             self.minio_client = boto3.client(
                 "s3",
@@ -71,7 +68,6 @@ class MinioConnection:
             logger.error(f"❌ MinIO 연결 실패: {e}")
 
     def list_buckets(self):
-        """MinIO 버킷 리스트 조회 (연결 실패와 버킷 없음 구분)"""
         if not self.minio_client:
             logger.error("❌ MinIO에 연결되지 않았습니다. 버킷 조회 불가!")
             return None  # 버킷 없음이 아니라 '연결 실패' 상태임을 명확히 함.
@@ -90,5 +86,5 @@ class MinioConnection:
             logger.error(f"❌ 버킷 조회 실패: {e}")
             return None
 
-    def get_object(self, bucket_name, object_name):
-        return self.minio_client.get_object(bucket_name, object_name)
+    def get_object(self, bucket, key):
+        return self.minio_client.get_object(Bucket=bucket, Key=key)
---
+++ blocking_app/main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from blocking_app.config.minio import MinioConnection
+from blocking_app.config.rabbitmq import RabbitMQConnection
+import json
+
+from blocking_app.config.custom_logger import logger
+
+load_dotenv()
+
+
+def process_message(minio_client, msg):
+    logger.info(f"📩 Received message: {msg}")
+    try:
+        data = json.loads(msg)
+        file_name = data["file_name"]
+        bucket_name = data["bucket"]
+
+        logger.info(f"🔍 Processing file: {file_name} from bucket: {bucket_name}")
+        try:
+            response = minio_client.get_object(bucket=bucket_name, key=file_name)
+            image_data = response["Body"].read()
+            response["Body"].close()
+
+            logger.info(
+                f"✅ Successfully fetched '{file_name}'. Size: {len(image_data)} bytes"
+            )
+
+        except Exception as e:
+            logger.error(f"❌ Error fetching image '{file_name}': {e}")
+
+    except json.JSONDecodeError as e:
+        logger.error(f"❌ JSON 파싱 오류: {e}")
+    except Exception as e:
+        logger.error(f"❌ 메시지 처리 중 오류 발생: {e}")
+
+
+def main():
+    minio_client = MinioConnection()
+    minio_client.list_buckets()
+    rabbitmq_consumer = RabbitMQConnection(auto_connect=True)
+    try:
+        rabbitmq_consumer.consume_messages(
+            callback=lambda msg: process_message(minio_client, msg)
+        )
+    except Exception as ex:
+        logger.error(f"❌ 오류 발생: {ex}")
+
+
+if __name__ == "__main__":
+    main()
